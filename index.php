f3ea5e15139d3d71b72af0fa6d4dd9fe9bbfe11d
Matteo Courquin <matteo.courquin@gmail.com>
2021-05-08 01:41:42 +0200

    sav

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 	<head>
 		<title>La Villa Savoye</title>
 		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Exo:wght@100;300;400;600&display=swap" rel="stylesheet">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<meta charset="utf-8">
 	</head>
