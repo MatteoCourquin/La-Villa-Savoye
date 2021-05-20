a545435a08615a792661fdecc25c3127b0676a48
Matteo Courquin <matteo.courquin@gmail.com>
2021-05-20 21:49:10 +0200

    install Jquery

--- script/script.js
+++ script/script.js
@@ -1,32 +1,3 @@
-//Je selectionne mes éléments :
-
-var titre = document.querySelectorAll('h1 span');
-var l1 = document.querySelector('#ligne1');
-var l2 = document.querySelector('#ligne2');
-var bouttons = document.querySelectorAll(".boutton");
-
-//Focntion ouvrir/fermer le menu :
-
-function afficherMenu() {
-	menu.classList.toggle('visible');
-}
-
-//Fonction de la time line qui fait apparaitre les éléments au chargement de la page :
-
-window.addEventListener('load', () => {
-
-	var TL = gsap.timeline({paused: true});
-
-	TL
-	.staggerFrom(titre, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
-	.from(l1, 1, {width: 0, ease: "power2.out"}, '-=1')
-	.from(l2, 1, {width: 0, ease: "power2.out"}, '-=1')
-	.staggerFrom(bouttons, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1');
-
-	TL.play();
-
-})
-
 //Menu burger :
 //Je selectionne mes éléments :
 
